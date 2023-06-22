ce923d2fb518bf1f9514f12634974f45f0d478f8
Chek <zzzxxxccc11225@yahoo.com.tw>
2023-06-23 03:10:32 +0800

    更換偵測程式

--- js/all.js
+++ js/all.js
@@ -1,141 +1,46 @@
-// V2
-// function startNfcScan() {
-//     if ('NDEFReader' in window) {
-//       const statusElement = document.getElementById('status');
-//       statusElement.innerHTML = '<i class="fas fa-spinner fa-spin"></i> 請掃描 NFC'; // 初始狀態：請掃描 NFC，並加上旋轉的 Spinner 圖示
+let nfc;
+let nfcDataContainer;
 
-//       const valueInput = document.getElementById('valueInput');
-//       valueInput.value = ''; // 清空數值框的值
-
-//       const reader = new NDEFReader();
-
-//       reader.addEventListener('reading', event => {
-//         const records = event.message.records;
-
-//         for (const record of records) {
-//           console.log(record.data);
-//           const decoder = new TextDecoder(record.encoding);
-//           const value = decoder.decode(record.data);
-
-//           // 將讀取到的值設定到數值框中
-//           valueInput.value = value;
-//         }
-
-//         statusElement.innerHTML = '<i class="fas fa-check"></i> NFC 狀態：已讀取'; // 讀取完成後的狀態：已讀取
-//       });
-
-//       reader.scan().catch(error => {
-//         console.error('Error scanning NFC: ', error);
-//       });
-//     } else {
-//       console.error('Web NFC API is not supported in this browser.');
-//       document.getElementById('status').innerHTML = '<i class="fas fa-times red"></i> NFC 功能不支援';
-//     }
-//   }
-
-// V3 狀態檢測版
-// function startNfcScan() {
-//   if ('NDEFReader' in window) {
-//     const statusElement = document.getElementById('status');
-//     statusElement.innerHTML = '<i class="fas fa-spinner fa-spin"></i> 請掃描 NFC'; // 初始狀態：請掃描 NFC，並加上旋轉的 Spinner 圖示
-
-//     const valueInput = document.getElementById('valueInput');
-//     valueInput.value = ''; // 清空數值框的值
-
-//     const formatElement = document.getElementById('format');
-//     formatElement.textContent = ''; // 清空格式顯示區域
-
-//     const reader = new NDEFReader();
-
-//     reader.addEventListener('reading', event => {
-//       const records = event.message.records;
-
-//       for (const record of records) {
-//         console.log("Record data:", record.data);
-//         const decoder = new TextDecoder(record.encoding);
-//         const value = decoder.decode(record.data);
-
-//         // 將讀取到的值設定到數值框中
-//         valueInput.value = value;
-
-//         // 檢查記錄類型並顯示格式
-//         if (record.recordType === "text") {
-//           console.log("Record type: Text");
-//           formatElement.textContent += "Text format\n";
-//         } else if (record.recordType === "url") {
-//           console.log("Record type: URL");
-//           formatElement.textContent += "URL format\n";
-//         } else {
-//           console.log("Custom record type:", record.recordType);
-//           formatElement.textContent += "Custom record type: " + record.recordType + "\n";
-//         }
-//       }
-
-//       statusElement.innerHTML = '<i class="fas fa-check"></i> NFC 狀態：已讀取'; // 讀取完成後的狀態：已讀取
-//     });
-
-//     reader.scan().catch(error => {
-//       console.error('Error scanning NFC: ', error);
-//     });
-//   } else {
-//     console.error('Web NFC API is not supported in this browser.');
-//     document.getElementById('status').innerHTML = '<i class="fas fa-times red"></i> NFC 功能不支援';
-//   }
-// }
-
-// V3.1
-function startNfcScan() {
-  if ('NDEFReader' in window) {
-    const statusElement = document.getElementById('status');
-    statusElement.innerHTML = '<i class="fas fa-spinner fa-spin"></i> 請掃描 NFC'; // 初始狀態：請掃描 NFC，並加上旋轉的 Spinner 圖示
-
-    const valueInput = document.getElementById('valueInput');
-    valueInput.value = ''; // 清空數值框的值
-
-    const formatElement = document.getElementById('format');
-    formatElement.innerHTML = ''; // 清空格式顯示區域的內容
-
-    const reader = new NDEFReader();
-
-    reader.addEventListener('reading', event => {
-      const records = event.message.records;
-
-      for (const record of records) {
-        console.log("Record data:", record.data);
-        const decoder = new TextDecoder(record.encoding);
-        const value = decoder.decode(record.data);
-
-        // 將讀取到的值設定到數值框中
-        valueInput.value = value;
-
-        // 檢查記錄類型並顯示格式
-        let recordType = '';
-        if (record.recordType === "text") {
-          console.log("Record type: Text");
-          recordType = 'Text';
-        } else if (record.recordType === "url") {
-          console.log("Record type: URL");
-          recordType = 'URL';
-        } else {
-          console.log("Custom record type:", record.recordType);
-          recordType = 'Custom';
-        }
-
-        // 在網頁上顯示格式
-        const recordElement = document.createElement('div');
-        recordElement.className = 'record';
-        recordElement.innerHTML = `Record type: ${recordType}`;
-        formatElement.appendChild(recordElement);
-      }
-
-      statusElement.innerHTML = '<i class="fas fa-check"></i> NFC 狀態：已讀取'; // 讀取完成後的狀態：已讀取
+// 初始化NFC
+async function initNFC() {
+  try {
+    nfc = new NDEFReader();
+    await nfc.scan();
+    
+    nfcDataContainer = document.getElementById('nfcDataContainer');
+    
+    nfc.addEventListener('reading', ({ message }) => {
+      nfcDataContainer.innerHTML = '';
+      
+      message.records.forEach(record => {
+        const p = document.createElement('p');
+        p.textContent = `資料類型：${record.recordType}, 資料內容：${record.data}`;
+        nfcDataContainer.appendChild(p);
+      });
     });
+    
+    console.log('NFC初始化完成');
+  } catch (error) {
+    console.error('無法初始化NFC:', error);
+  }
+}
 
-    reader.scan().catch(error => {
-      console.error('Error scanning NFC: ', error);
-    });
+// 開始讀取NFC資料
+function startReading() {
+  if (nfc) {
+    nfc.start();
+    console.log('開始讀取NFC資料');
   } else {
-    console.error('Web NFC API is not supported in this browser.');
-    document.getElementById('status').innerHTML = '<i class="fas fa-times red"></i> NFC 功能不支援';
+    console.error('NFC未初始化');
+  }
+}
+
+// 停止讀取NFC資料
+function stopReading() {
+  if (nfc) {
+    nfc.stop();
+    console.log('停止讀取NFC資料');
+  } else {
+    console.error('NFC未初始化');
   }
 }
